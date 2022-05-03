cd7132c78ac2598aa95559c2734b259425ebc1dd
BronWhyte <bronwyn.whyte24@gmail.com>
2022-05-03 16:01:53 +0100

    Created weather layout and integrated weather API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import ReactAnimatedWeather from "react-animated-weather";
 
 function App() {
-  return (
+  const [city, setCity] = useState(null);
+  const [name, setName] = useState(null);
+  const [weather, setWeather] = useState({});
+  const [loaded, setLoaded] = useState(false);
+  let form = (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Enter a city"
+          onChange={handleCity}
+        ></input>
+        <input type="submit" value="Search"></input>
+      </form>
+      <h1>{name}</h1>
     </div>
   );
+
+  function updateWeather(response) {
+    setLoaded(true);
+    setName(response.data.name);
+    setWeather({
+      description: response.data.weather[0].description,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+    });
+  }
+
+  function handleCity(event) {
+    setCity(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "515c9ddbeb3cda9061acfab71031839e";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+    axios.get(apiUrl).then(updateWeather);
+  }
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Tuesday 14:15</li>
+          <li>{weather.description}</li>
+          <li>Precipitation: 69%</li>
+          <li>Wind: {weather.wind} km/h</li>
+          <li>
+            <ReactAnimatedWeather
+              icon={"CLEAR_DAY"}
+              color={"white"}
+              size={64}
+              animate={false}
+            />
+            <span className="temperature">
+              {Math.round(weather.temperature)}
+            </span>
+            <span className="degreeSymbol">°C</span>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
